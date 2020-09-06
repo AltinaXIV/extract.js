58511810a59c6aeb295a6b43d76d932e6e360ccd
Giovanni Panaro <tsrwebgl@gmail.com>
2020-09-06 13:08:15 -0400

    Resuming project and updating deps.

--- src/extract.js
+++ src/extract.js
@@ -2,13 +2,21 @@ import * as Identify from "./identify";
 import * as Enums from "./enums"
 
 /**
+ * Get the signature of a section of a zip file
+ * @param {Int8Array} signatureArray 64bit signature as an 8 size array of 1 byte ints.
+ * @return {number} the 64 bit signature
+ */
+function getSignature(signatureArray) {
+
+}
+
+/**
  * Main function
- * @param {String} file The location of the file in which to do some action on
+ * @param {ArrayBuffer} file The location of the file in which to do some action on
  * @param {number} action the action to preform on the file
  */
 function process(file, action) {
-    // Only identify the file format if we are listing files OR extracting files
-    if(action === Enums.Action.LIST || action === Enums.Action.EXTRACT) {
-        const format = Identify.identifyCompressionFormat(file)
-    }
+    const fileContents = new Int8Array(file);
+    let position = 0;
+    getSignature();
 }
--- src/identify.js
+++ src/identify.js
@@ -2,7 +2,7 @@ import * as Enums from './enums'
 
 /**
  * Identify the format of a given file
- * @param {String} file location of file to identify format of
+ * @param {String} file - location of file to identify format of
  * @returns {number} the format of the file provided
  */
 export function identifyCompressionFormat(file) {
