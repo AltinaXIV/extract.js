4f6ed24788b676aa0c7a9f45ccfcdc7bd6127344
Giovanni Panaro <tsrwebgl@gmail.com>
2020-09-21 13:41:47 -0400

    Testing improvements

--- test/extract.test.js
+++ test/extract.test.js
@@ -1,7 +1,5 @@
-import * as Extract from '../src/extract'
-import * as jest from "m";
+let Extract = require("../src/extract");
 
-//const mockConvertArrayIntoInt = jest.fn(x => 0x04034b50);
 
 test("ensures that Int8arrays are turned into integers correctly (test 1)", () => {
     let arr = Uint8Array.of(0x2f, 0xa1);
@@ -16,13 +14,15 @@ test("ensures that Int8arrays are turned into integers correctly (test 2)", () =
 });
 
 test("ensures that readSignatures correctly returns a good signature", () => {
+    let spy = jest.spyOn(Extract, 'convertArrayIntoInt').mockReturnValueOnce(0x04034b50);
     let arr = Uint8Array.of(0x04, 0x03, 0x4b, 0x50);
     let expected = 0x04034b50;
     expect(Extract.getSignature(arr, 0)).toBe(expected);
 });
 
-test("ensures Enums.Signaturs.BAD_SIGNATURE is thrown when a bad signature is provided", () => {
+test("ensures Enums.Signatures.BAD_SIGNATURE is thrown when a bad signature is provided", () => {
+    let spy = jest.spyOn(Extract, 'convertArrayIntoInt').mockReturnValueOnce(0x12344567);
     let arr = Uint8Array.of(0x12, 0x34, 0x45, 0x67);
     let expected = 0;
-    expected(Extract.getSignature(arr, 0)).toBe(expected);
+    expect(Extract.getSignature(arr, 0)).toBe(expected);
 });
